750b8671aa28a09fd6a3f2773a5a30e23d0b210c
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-03-16 09:39:42 -0400

    add script2 with steps for creating exports

---
+++ inst/scripts/script2.R
@@ -0,0 +1,10 @@
+## Steps
+##
+## Import with physiologies
+## Group by and combine taxon names by taxids (combine synonyms).
+## Use a first ASR step to get the NCBI_ID of taxa with missing taxid.
+## Filter, keeping only row wit taxid.
+## Resolve any conflicts or agreements.
+## Run ASR/Inheritance  .
+## Create new data.frames.
+## Combine data.frames and export the full_dump file.
