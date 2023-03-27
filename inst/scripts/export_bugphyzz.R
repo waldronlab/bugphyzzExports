b75f8af1d34c440aa708927f9163fe33daf4ad17
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-03-27 10:15:45 -0400

    add some code for preparing data for propagation

--- inst/scripts/export_bugphyzz.R
+++ inst/scripts/export_bugphyzz.R
@@ -1,6 +1,9 @@
+
+library(taxPPro)
 library(bugphyzz)
 library(bugsigdbr)
 library(readr)
+library(purrr)
 
 # Lower bounds are excluded but upper bounds are included
 
@@ -9,7 +12,7 @@ library(readr)
         "small" = list(lower = NA, upper = 0.7),
         "moderate" = list(lower = 0.71, upper = 7.2),
         "large" = list(lower = 7.21, upper = 47.9)
-    ), 
+    ),
     "butyrate producing" = list(
         "low" = list(lower = NA, upper = 4.9),
         "medium" = list(lower = 4.91, upper = 12.7),
@@ -18,7 +21,7 @@ library(readr)
     "coding genes" = list(
         "very small" = list(lower = NA, upper = 473),
         "small" = list(lower = 474, upper = 600),
-        "average" = list(lower = 601, upper = 6000), 
+        "average" = list(lower = 601, upper = 6000),
         "very large"= list(lower = 6001, upper = 999999999999)
     ),
     "fatty acid" = list(
@@ -87,7 +90,7 @@ library(readr)
 #' Returns if a physiology has manually curated thresholds
 #'
 #' @param physiology the name of a physiology
-#' 
+#'
 #' @return boolean
 #'
 #' @examples
@@ -99,7 +102,7 @@ library(readr)
 #' Returns a physiology's manually curated thresholds
 #'
 #' @param physiology the name of a physiology
-#' 
+#'
 #' @return list of lists representing threshold ranges
 #'
 #' @examples
@@ -109,14 +112,14 @@ library(readr)
 }
 
 #' Get list of physiology signatures by attributes and attribute values
-#' 
+#'
 #' @param physiology data frame from bugphyzz
 #' @param tax.id.type "Taxon_name" or "NCBI_ID"
 #' @param tax.level "species" or "genus"
 #' @return list of signature lists
-#' 
+#'
 #' @importFrom bugphyzz makeSignatures
-#' 
+#'
 #' @example
 #' ps <- physiologies()
 #' p <- ps$`animal pathogen`
@@ -163,8 +166,8 @@ library(readr)
             thresholds <- .getSpecialThresholds(p)
             for (threshold in names(thresholds)) {
                 lower_bound <- thresholds[[threshold]]$lower
-                upper_bound <- thresholds[[threshold]]$upper  
-                signatures <- append(signatures, 
+                upper_bound <- thresholds[[threshold]]$upper
+                signatures <- append(signatures,
                                      .getSignaturesByThreshold(p,
                                                                tax.id.type,
                                                                tax.level,
@@ -181,9 +184,9 @@ library(readr)
 }
 
 #' Header for bugphyzz files
-#' 
+#'
 #' @param identifier any character vector. Defaults to today.
-#' 
+#'
 #' @examples
 #' .getHeader("3.17")
 .getHeader <- function(identifier = format(Sys.time(), "%Y-%m-%d")) {
@@ -196,7 +199,7 @@ library(readr)
 #'
 #' @param file_path path to the file
 #' @param header a character vector representing the header
-#' 
+#'
 #' @importFrom readr read_lines write_lines
 #'
 #' @examples
@@ -207,14 +210,14 @@ library(readr)
 }
 
 #' Write file given a signature with header
-#' 
+#'
 #' @param signatures to write to file
 #' @param file_path to write a file
 #' @param header a character vector representing the header
 #'
 #' @importFrom bugsigdbr writeGMT
 #' @importFrom readr write_csv
-#' 
+#'
 #' @examples
 #' writeFileWithHeader(signatures, "mysigs.gmt", .getHeader())
 .writeFileWithHeader <- function(signatures, file_path, header = header) {
@@ -223,7 +226,7 @@ library(readr)
 }
 
 #' Write a full dump csv
-#' 
+#'
 #' Include only similar columns
 #'
 #' @param file_path to write a file
@@ -240,21 +243,21 @@ library(readr)
     all_ps_ci <- lapply(ps, function(p) p[colname_intersection])
     for (i in 1:length(all_ps_ci)) {
         write_csv(all_ps_ci[[i]], file_path, col_names = i == 1, append = i != 1)
-    }    
+    }
     .writeHeader(file_path, header)
 }
 
 #' Make all signatures
-#' 
+#'
 #' Write four files for the combinations of .TAX.LEVELS and .TAX.ID.TYPES
 #'
 #' @param header a character vector representing the header. Use default header
 #' with date.
-#' 
+#'
 #' @importFrom bugphyzz fattyAcidComposition physiologies
-#' 
+#'
 #' @export
-#' 
+#'
 #' @examples
 #' makeAllSignatures()
 makeAllSignatures <- function(header = .getHeader()) {
@@ -275,4 +278,31 @@ makeAllSignatures <- function(header = .getHeader()) {
     .makeFullDump("full_dump.csv", ps, header = header)
 }
 
-makeAllSignatures()
+# makeAllSignatures()
+
+phys <- physiologies(keyword = 'all', remove_false = TRUE, full_source = FALSE)
+
+## Removing these for now
+phys$`metabolite production` <- NULL
+phys$`metabolite utilization` <- NULL
+
+data_ready <- vector('list', length(phys))
+for (i in seq_along(data_ready)) {
+    message('Preparaing ', names(phys)[i], ' - ', Sys.time())
+    names(data_ready)[i] <- names(phys)[i]
+    res <- tryCatch(
+        error = function(e) NULL,
+        {
+            prepareDatForPropagation(phys[[i]])
+        }
+    )
+    if (!is.null(res)) {
+        data_ready[[i]] <- res
+    }
+}
+vct_lgl <- map_lgl(data_ready, is.null)
+if (any(vct_lgl)) {
+    message('Errors in ', sum(vct_lfl), 'data.frames.')
+    data_ready <- discard(data_ready, is.null)
+}
+
