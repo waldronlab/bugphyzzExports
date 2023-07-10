b291733340db3012c54b6e9261ef01339607a3db
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-07-10 17:13:21 -0400

    update full_dump file

--- inst/scripts/export_bugphyzz.R
+++ inst/scripts/export_bugphyzz.R
@@ -18,26 +18,26 @@ phys_names <- c(
     "animal pathogen",
     "antimicrobial resistance",
     "antimicrobial sensitivity",
-    # "arrangement",
-    # "biofilm forming",
-    # "biosafety level",
+    "arrangement",
+    "biofilm forming",
+    "biosafety level",
     "butyrate producing",
     # "COGEM pathogenicity rating",
     # "country",
-    # "disease association",
-    # "extreme environment",
+    "disease association",
+    "extreme environment",
     # "geographic location",
     "gram stain",
-    # "growth medium",
+    "growth medium",
     # "habitat",
     "health associated",
-    # "hemolysis",
+    "hemolysis",
     "hydrogen gas producing",
     # "isolation site",
     "lactate producing",
     # "metabolite production",
     # "metabolite utilization",
-    # "motility",
+    "motility",
     "pathogenicity human",
     "plant pathogenicity",
     "shape",
@@ -47,13 +47,13 @@ phys_names <- c(
 
     ## Ranges <<<<
 
-    # "coding genes",
-    # "genome size",
+    "coding genes",
+    "genome size",
     "growth temperature",
     "halophily",
     "length",
-    # "mutation rate per site per generation",
-    # "mutation rates per site per year",
+    "mutation rate per site per generation",
+    "mutation rates per site per year",
     "optimal ph",
     "width"
 )
@@ -93,23 +93,6 @@ data_ready <- bplapply(data, function(x) {
 })
 data_ready <- discard(data_ready, is_error)
 
-# data_ready <- vector('list', length(data))
-# for (i in seq_along(data_ready)) {
-#     message('Preparing ', names(data)[i], '.')
-#     names(data_ready)[i] <- names(data)[i]
-#     data_ready[[i]] <- tryCatch(
-#         error = function(e) e,
-#         {
-#             data[[i]] |>
-#                 prepareDataForPropagation() |>
-#                 mergeOriginalAndEarlyASR() |>
-#                 group_by(NCBI_ID) |>
-#                 mutate(Score = Score / sum(Score)) |>
-#                 ungroup() |>
-#                 distinct()
-#         }
-#     )
-# }
 data('tree_list')
 tree <- as.Node(tree_list)
 
@@ -159,68 +142,23 @@ propagated <- bplapply(X = data_ready, BPPARAM = MulticoreParam(workers = 16), F
     return(final_table)
 })
 
-
-
-
-
-
-
-
-# message('>>>>>>> Propagating data ', Sys.time(), ' <<<<<<')
-# data('tree_list')
-# tree <- as.Node(tree_list)
-# propagated <- vector('list', length(data_ready))
-# for (i in seq_along(propagated)) {
-#     input_tbl <- data_ready[[i]] |>
-#         select(NCBI_ID, Attribute, Score, Evidence) |>
-#         distinct() |>
-#         tidyr::complete(
-#             NCBI_ID, Attribute, fill = list(Score = 0, Evidence = '')
-#         )
-#     l <- split(input_tbl, factor(input_tbl$NCBI_ID))
-#     tree$Do(function(node) {
-#         if (!is.null(l[[node$name]])) {
-#             node[['table']] <- l[[node$name]]
-#         }
-#     })
-#     tree$Do(asr, traversal = 'post-order')
-#     tree$Do(inh, traversal = 'pre-order')
-#
-#     data_tree_tbl <- tree$Get(function(node) node[['table']], simplify = FALSE) |>
-#         purrr::discard(~ all(is.na(.x))) |>
-#         dplyr::bind_rows() |>
-#         relocate(NCBI_ID)
-#
-#     attrs <- unique(data[[i]]$Attribute)
-#     all_node_names <- tree$Get(function(node) node$name, simplify = FALSE) |>
-#         unlist(recursive = TRUE, use.names = FALSE) |>
-#         unique()
-#     all_node_names <- all_node_names[which(!all_node_names %in% unique(data_tree_tbl$NCBI_ID))]
-#     empty_df <- data.frame(
-#         NCBI_ID = sort(rep(all_node_names, length(attrs))),
-#         Attribute = rep(attrs, length(all_node_names)),
-#         Score = 0,
-#         Evidence = NA
-#     )
-#     inferred_values <- bind_rows(data_tree_tbl, empty_df)
-#
-#     other_ids <- data[[i]]$NCBI_ID[which(!phys$NCBI_ID %in% inferred_values$NCBI_ID)]
-#     other_ids <- unique(other_ids)
-#     other_phys <- filter(data[[i]], NCBI_ID %in% other_ids)
-#     final_table <- bind_rows(inferred_values, other_phys)
-#     propagated[[i]] <- final_table
-#
-#     tree$Do(function(node) {
-#         node[['table']] <- NULL
-#     })
-# }
-# names(propagated) <- names(data_ready)
-
 full_dump <- bind_rows(propagated)
 full_dump$NCBI_ID <- sub('^[dpcofgst]__', '', full_dump$NCBI_ID)
 full_dump$Attribute <- gsub(' ', '_', full_dump$Attribute)
+
 full_dump_fname <- paste0("full_dump.csv.bz2")
-unlink(full_dump_fname)
-con <- bzfile(full_dump_fname, "w")
-write.csv(x = full_dump, file = con, quote = TRUE, row.names = FALSE)
-close(con)
+on.exit(unlink(full_dump_fname))
+# con <- bzfile(full_dump_fname, "w")
+# write.csv(x = full_dump, file = con, quote = TRUE, row.names = FALSE)
+readr::write_csv(x = full_dump, file = full_dump_fname, quote = 'needed', num_threads = 16)
+# close(con)
+
+
+readr::write_csv(x = full_dump, file = "full_dump.csv.bz2", quote = 'needed', num_threads = 16)
+
+
+
+
+
+
+
