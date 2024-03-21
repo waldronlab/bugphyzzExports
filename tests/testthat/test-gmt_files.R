4960f70d513d95c6c7150b89e572e41fe9d4c099
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-03-21 13:51:01 -0400

    Add tests more tests for GMT files.

--- tests/testthat/test-gmt_files.R
+++ tests/testthat/test-gmt_files.R
@@ -1,4 +1,5 @@
 library(qusage)
+library(purrr)
 expected_file_names <- c(
         "bugphyzz-genus-NCBI_ID.gmt", "bugphyzz-genus-Taxon_name.gmt",
         "bugphyzz-species-NCBI_ID.gmt", "bugphyzz-species-Taxon_name.gmt",
@@ -6,7 +7,10 @@ expected_file_names <- c(
         "bugphyzz-mixed-NCBI_ID.gmt", "bugphyzz-mixed-Taxon_name.gmt"
     )
 fnames <- list.files(path = "../", pattern = "gmt")
-sigs <- lapply(fnames, read.gmt)
+sigs <- map(fnames, read.gmt)
+names(sigs) <- fnames
+## First line is a comment that should be removed
+sigs <- map(sigs, function(x) discard(x, ~ !length(.x)))
 
 test_that("All gmt files are present", {
     res <- all(fnames %in% expected_file_names)
@@ -15,9 +19,29 @@ test_that("All gmt files are present", {
 
 test_that("All gmt files can be read", {
     expect_no_error({
-        gmt_files <- lapply(fnames, read.gmt)
+        gmt_files <- map(fnames, read.gmt)
     })
     expect_no_warning({
-        gmt_files <- lapply(fnames, read.gmt)
+        gmt_files <- map(fnames, read.gmt)
     })
+})
+
+test_that("NCBI_ID-GMT signatures can be converted to integers", {
+    ncbi_sigs <- sigs[grep("NCBI_ID", names(sigs))]
+    ncbi_sigs <- list_flatten(ncbi_sigs)
+    are_int <- map_lgl(ncbi_sigs, ~ {
+        integ <- as.integer(.x)
+        any(is.na(integ))
+    })
+    expect_false(any(are_int))
+})
+
+test_that("Taxon_name-GMT signatures contain letters", {
+    name_sigs <- sigs[grep("Taxon_name", names(sigs))]
+    name_sigs <- list_flatten(name_sigs)
+    are_ltrs <- map_lgl(name_sigs, ~ {
+       ltrs <- strsplit(.x, "")[[1]]
+        any(ltrs %in% letters) | any(ltrs %in% LETTERS)
+    })
+    expect_true(all(are_ltrs))
 })
