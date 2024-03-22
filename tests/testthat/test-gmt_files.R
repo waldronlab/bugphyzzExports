5951d0cf39eac42f98589cb6a1597e3ba2704fe4
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-03-21 20:57:43 -0400

    Add tets for reading CSV files.

--- tests/testthat/test-gmt_files.R
+++ tests/testthat/test-gmt_files.R
@@ -1,46 +1,57 @@
-library(qusage)
+
+# Setup -------------------------------------------------------------------
+
+library(clusterProfiler)
 library(purrr)
-expected_file_names <- c(
+path_to_files <- "../../" # Only for linux
+checkFnamesGMT <- function(x) {
+    if (!length(x))
+        return(FALSE)
+    expected_file_names <- c(
         "bugphyzz-genus-NCBI_ID.gmt", "bugphyzz-genus-Taxon_name.gmt",
+        "bugphyzz-mixed-NCBI_ID.gmt",  "bugphyzz-mixed-Taxon_name.gmt",
         "bugphyzz-species-NCBI_ID.gmt", "bugphyzz-species-Taxon_name.gmt",
-        "bugphyzz-strain-NCBI_ID.gmt", "bugphyzz-strain-Taxon_name.gmt",
-        "bugphyzz-mixed-NCBI_ID.gmt", "bugphyzz-mixed-Taxon_name.gmt"
+        "bugphyzz-strain-NCBI_ID.gmt", "bugphyzz-strain-Taxon_name.gmt"
     )
-fnames <- list.files(path = "../", pattern = "gmt")
-sigs <- map(fnames, read.gmt)
-names(sigs) <- fnames
-## First line is a comment that should be removed
-sigs <- map(sigs, function(x) discard(x, ~ !length(.x)))
+    fnames_ <- sub("^.*/bug", "bug", x)
+    all(expected_file_names == fnames_)
+}
+canReadGMT <- function(x) {
+    if (!length(x))
+        stop("Empty vector", call. = FALSE)
+    purrr::map(x, clusterProfiler::read.gmt)
+}
+
+# Tests -------------------------------------------------------------------
+
+fnames <- list.files(path = path_to_files, pattern = "gmt", full.names = TRUE)
 
 test_that("All gmt files are present", {
-    res <- all(fnames %in% expected_file_names)
-    expect_true(res)
+    expect_true(checkFnamesGMT(fnames))
 })
 
 test_that("All gmt files can be read", {
     expect_no_error({
-        gmt_files <- map(fnames, read.gmt)
-    })
-    expect_no_warning({
-        gmt_files <- map(fnames, read.gmt)
+        x <- canReadGMT(fnames)
     })
 })
 
+dats <- canReadGMT(fnames)
+names(dats) <- fnames
+
 test_that("NCBI_ID-GMT signatures can be converted to integers", {
-    ncbi_sigs <- sigs[grep("NCBI_ID", names(sigs))]
-    ncbi_sigs <- list_flatten(ncbi_sigs)
-    are_int <- map_lgl(ncbi_sigs, ~ {
-        integ <- as.integer(.x)
+    ncbi_dats <- dats[grep("NCBI_ID", names(dats))]
+    are_int <- map_lgl(ncbi_dats, ~ {
+        integ <- as.integer(.x$gene)
         any(is.na(integ))
     })
     expect_false(any(are_int))
 })
 
 test_that("Taxon_name-GMT signatures contain letters", {
-    name_sigs <- sigs[grep("Taxon_name", names(sigs))]
-    name_sigs <- list_flatten(name_sigs)
-    are_ltrs <- map_lgl(name_sigs, ~ {
-       ltrs <- strsplit(.x, "")[[1]]
+    name_dats <- dats[grep("Taxon_name", names(dats))]
+    are_ltrs <- map_lgl(name_dats, ~ {
+       ltrs <- strsplit(.x$gene, "")[[1]] # just check the first element
         any(ltrs %in% letters) | any(ltrs %in% LETTERS)
     })
     expect_true(all(are_ltrs))
